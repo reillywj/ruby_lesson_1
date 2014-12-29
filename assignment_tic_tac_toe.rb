3f8a3c78ba1ed5afd2f7b1c035ed416d87b6e413
William Reilly <william.reilly2010@gmail.com>
2014-12-29 03:27:52 +0000

    Started Tic Tac Toe.rb

---
+++ assignment_tic_tac_toe.rb
@@ -0,0 +1,16 @@
+#assignment_tic_tac_toe.rb
+#Tic Tac Toe
+#Player Chooses where to go.
+#Computer plays (where player has not played)
+#Keeps playing until winner or no more spaces to go
+
+
+# Show tic tac toe board
+def show_board(pos_hash)
+end
+# Ask Player where to go (make sure valid entry), check if won?
+# Computer plays, check if won? (random; 2ndary goal to build to always bloc; tertiary goal to never lose)
+# Check if any places to go, if none, exit loop (tie)
+
+tic_tac_toe_board = {'1' => ' ', '2' => ' ', '3' => ' ', '4' => ' ', '5' => ' ', '6' => ' ', '7' => ' ', '8' => ' ', '9' => ' '}
+puts tic_tac_toe_board.to_s
