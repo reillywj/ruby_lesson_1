628bb39b20d1a9d200cb40c5b9b959478a08494b
William Reilly <william.reilly2010@gmail.com>
2014-12-31 22:01:14 +0000

    Finished black jack game.

--- assignment_blackjack.rb
+++ assignment_blackjack.rb
@@ -88,7 +88,7 @@ def show_first_card(card_array)
 end
 
 def hit_me?
-  say "Would you like to hit? hit: 'y' or stay: 'n'"
+  say "hit: 'y' or stay: 'n'"
   answer = gets.chomp.downcase
   if answer == 'y'
     true
@@ -115,13 +115,23 @@ end
 
 def calculate_value_of(cards)
   value_of_cards = 0
+  number_of_aces = 0
   cards.each do |card|
     card_type = card[1]
     value_of_card_type = CARD_VALUE_HASH[card_type]
-    if (card_type == "ace" && (value_of_cards + value_of_card_type > 21))
-      value_of_cards += 1
+    if (card_type == "ace")
+      number_of_aces += 1
     else
       value_of_cards += value_of_card_type
+    end
+  end
+  if number_of_aces > 0
+    for i in 1..number_of_aces do
+      if value_of_cards + (11 * (number_of_aces - i + 1)) > 21
+        value_of_cards += 1
+      else
+        value_of_cards += 11
+      end
     end
   end
   value_of_cards
@@ -129,10 +139,10 @@ end
 
 def show_cards_player_playing(player_cards, dealer_cards)
   say_title "Your Turn"
-  say "Your cards"
+  say_title "Your Cards"
   show_all_cards player_cards
   say_value_of_cards("Your", player_cards)
-  say "Dealer's cards"
+  say_title "Dealer's Cards"
   show_first_card dealer_cards
 end
 
@@ -142,10 +152,10 @@ end
 
 def show_cards_dealer_playing(player_cards, dealer_cards)
   say_title "Dealer's Turn"
-  say "Your cards"
+  say_title "Your cards"
   show_all_cards player_cards
   say_value_of_cards("Your", player_cards)
-  say "Dealer's cards"
+  say_title "Dealer's cards"
   show_all_cards dealer_cards
   say_value_of_cards("The Dealer's", dealer_cards)
 end
@@ -158,6 +168,15 @@ def say_current_bank(bank_amount)
   say "You currently have #{bank_amount} in your bank."
 end
 
+def sleep_and_clear_system(time)
+  sleep_for 0.0 #time
+  system "clear"
+end
+
+def say_player_and_dealer_cards(player_cards, dealer_cards)
+  say "You have #{player_cards}. The dealer has #{dealer_cards}."
+end
+
 #-------------------------------------------------------------------------------
 
 SUITS = ["hearts", "diamonds", "spades", "clubs"]
@@ -167,15 +186,17 @@ CARD_VALUE_HASH = {"ace" =>11, "2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6,
 
 play_again = initialize_keep_playing
 
+sleep_and_clear_system 0.5
 say_title "Black Jack"
 player_name = get_player_name
+sleep_and_clear_system 1.0
 say "Hello, #{player_name}! We're about to play some Black Jack."
-sleep_for 0.5
 
 begin
+  sleep_and_clear_system 2.0
   player_bank = initialize_player_bank
   keep_playing = initialize_keep_playing
-  say_title "Let's play some Black Jack"
+  say_title "#{player_name}, let's play some Black Jack"
   begin
     deck = reset_deck(SUITS, CARDS)
     player_cards = []
@@ -185,7 +206,7 @@ begin
     hit_variable = true
     bust_variable = false
     
-    say_title "Let's bet"
+    say_title "What's your bet?"
     current_bet = get_player_bet player_bank
     say "You bet #{current_bet}."
     
@@ -195,15 +216,15 @@ begin
     player_cards << deal_card(deck)
     dealer_cards << deal_card(deck)
     player_value = calculate_value_of(player_cards)
-    p player_value
     dealer_value = calculate_value_of(dealer_cards)
-    p dealer_value
-    #Show cards dealt
-    show_cards_player_playing(player_cards, dealer_cards)
+    say_title "Dealing Cards..."
+    sleep_and_clear_system 5.0
     
     #Player hit or stay?
     until player_value >= 21 || !hit_variable
       system "clear"
+      show_cards_player_playing(player_cards, dealer_cards)
+      say_title "Hit or stay?"
       hit_variable = hit_me?
       if player_value < 21 && hit_variable
         player_cards << deal_card(deck)
@@ -211,53 +232,57 @@ begin
         player_value = calculate_value_of(player_cards)
       end
     end
-
+    
+    sleep_and_clear_system 0.5
     show_cards_dealer_playing(player_cards, dealer_cards)
     if player_value < 21 && dealer_value < 17
       until dealer_value >= 17
-        # sleep_for 0.75
+        sleep_and_clear_system 0.25
         dealer_cards << deal_card(deck)
         dealer_value = calculate_value_of(dealer_cards)
         show_cards_dealer_playing(player_cards, dealer_cards)
+        sleep_and_clear_system 0.5
       end
     end
     
     #Shouldn't need following, but adding in redundancy for time being.
-    player_value  = calculate_value_of player_cards
-    dealer_value = calculate_value_of dealer_cards
-    say_title("Award?")
-    say "Player_value: #{player_value}"
-    say "Dealer_value: #{dealer_value}"
+    say_title("Hand Over")
     #Award winnings
     if player_value > 21
+      say_player_and_dealer_cards(player_cards, dealer_cards)
       say "You busted with #{player_value}. You lost #{current_bet}."
       player_bank -= current_bet
       say_current_bank player_bank
     elsif (player_value < dealer_value) && (dealer_value <= 21)
+      say_player_and_dealer_cards(player_cards, dealer_cards)
       say "The dealer beat you #{dealer_value} to #{player_value}. You lost #{current_bet}."
       player_bank -= current_bet
       say_current_bank player_bank
     elsif player_value == 21
+      say_player_and_dealer_cards(player_cards, dealer_cards)
       say "You hit #{player_value} BLACK JACK! You won #{current_bet}."
       player_bank += current_bet
       say_current_bank player_bank
     elsif player_value > dealer_value
+      say_player_and_dealer_cards(player_cards, dealer_cards)
       say "You beat the dealer #{player_value} to #{dealer_value}. You won #{current_bet}!"
       player_bank += current_bet
       say_current_bank player_bank
     elsif dealer_value > 21
+      say_player_and_dealer_cards(player_cards, dealer_cards)
       say "The dealer busted with #{dealer_value}. You won #{current_bet}!"
       player_bank += current_bet
-      say_current_ank player_bank
+      say_current_bank player_bank
     elsif player_value == dealer_value
-      say "You had #{player_cards} and the Dealer had #{dealer_cards}."
+      say_player_and_dealer_cards(player_cards, dealer_cards)
       say "It's a tie: #{dealer_value} to #{player_value}. You did not lose any money!"
-    
     else
-      say player_value
-      say dealer_value
+      #Shouldn't need
+      say_title "Something's wrong in the win/loss logic."
+      show_cards_dealer_playing(player_cards, dealer_cards)
       say player_bank
     end
+    sleep_for 2.0
     
     #Play again?
     if player_bank == 0
@@ -272,8 +297,15 @@ begin
     end
   end while keep_playing
   say_title("GAME OVER")
-  say "You left with #{player_bank} in your pocket."
+  say "You left the table with #{player_bank} in your pocket."
   play_again = repeat? "play again"
+  if play_again
+    say_title("#{player_name}, you are about to play again.")
+    say_title "LOADING..."
+    sleep_for 1.0
+    say_title "LOADING......"
+    sleep_and_clear_system 2.0
+  end
 end while play_again
 
 say_title "Done Playing" 
