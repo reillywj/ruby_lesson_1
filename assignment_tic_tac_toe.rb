6f084626ea6199e6631d092d29f004d2a3d94f80
William Reilly <william.reilly2010@gmail.com>
2014-12-31 13:51:36 +0000

    Let player play again. Added delay feature.

--- assignment_tic_tac_toe.rb
+++ assignment_tic_tac_toe.rb
@@ -3,9 +3,8 @@
 #Player Chooses where to go.
 #Computer plays (where player has not played)
 #Keeps playing until winner or no more spaces to go
+#Ask to play again
 
-require 'pry'
-# Show tic tac toe board
 def show_board(board_hash)
   five_spaces = "#{" "*5}"
   five_dashes = "#{"-"*5}"
@@ -58,19 +57,20 @@ def return_possible_positions(board_hash)
   board_hash.select {|position, marker| marker =~ /[1-9]/}
 end
 
-def best_position_array(board_hash, winning_position_array, marker_to_block)
-  marker_position_array = return_marker_positions(marker_to_block, board_hash).keys
-  best_pos_array = []
-  location_array = []
-  winning_position_array.each_index {|index| location_array[index] = ((winning_position_array[index] & marker_position_array).count == 3)}
+# My attempt at doing some filtering out for best position
+# def best_position_array(board_hash, winning_position_array, marker_to_block)
+#   marker_position_array = return_marker_positions(marker_to_block, board_hash).keys
+#   best_pos_array = []
+#   location_array = []
+#   winning_position_array.each_index {|index| location_array[index] = ((winning_position_array[index] & marker_position_array).count == 3)}
   
-  winning_position_array.each_index {|index| }
-  if position_hash.values.count(marker_to_block) == 2
-    position_hash.select {|position, marker| marker =~ /[1-9]/}.keys.first
-  else
-    false
-  end
-end
+#   winning_position_array.each_index {|index| }
+#   if position_hash.values.count(marker_to_block) == 2
+#     position_hash.select {|position, marker| marker =~ /[1-9]/}.keys.first
+#   else
+#     false
+#   end
+# end
 
 def select_board(board_hash, array_of_positions_to_select)
   board_hash.select {|position, value| array_of_positions_to_select.include?(position)}
@@ -86,35 +86,43 @@ end
 
 WINNING_POSITIONS = [["1","2","3"], ["4","5","6"], ["7","8","9"],["1","4","7"],["2","5","8"],["3","6","9"],["1","5","9"],["3","5","7"]]
 
-player_winner = false
-computer_winner = false
-position_board_array = initialize_board(WINNING_POSITIONS)
-tic_tac_toe_board = [position_board_array, position_board_array].transpose.to_h
-
 begin
-  system "clear"
-  tell_player("#{"-"*20}YOUR MOVE#{"-"*20}")
-  show_board(tic_tac_toe_board)
-  possible_positions = return_possible_positions(tic_tac_toe_board)
-  tell_player("-"*49)
-  player_move = ask_player_for_move(possible_positions)
-  tic_tac_toe_board[player_move] = "X"
-  player_winner = is_winner?("X", tic_tac_toe_board, WINNING_POSITIONS)
-  unless player_winner
-    possible_positions = return_possible_positions(tic_tac_toe_board)
-    computer_move = possible_positions.keys.sample
-    tic_tac_toe_board[computer_move] = "O"
-    computer_winner = is_winner?("O", tic_tac_toe_board, WINNING_POSITIONS)
-  end
-end until return_possible_positions(tic_tac_toe_board).empty? || player_winner || computer_winner
+  player_winner = false
+  computer_winner = false
+  position_board_array = initialize_board(WINNING_POSITIONS)
+  tic_tac_toe_board = [position_board_array, position_board_array].transpose.to_h
+  play_again = true
 
-system "clear"
-tell_player("#{"-"*20}GAME OVER#{"-"*20}")
-if player_winner
-  tell_player("The computer doesn't know how to play. You won!")
-elsif computer_winner
-  tell_player("The computer got lucky. You lost!!!")
-else
-  tell_player("It's a tie.")
-end
-show_board(tic_tac_toe_board)
+  begin
+    system "clear"
+    tell_player("#{"-"*20}YOUR MOVE#{"-"*20}")
+    show_board(tic_tac_toe_board)
+    possible_positions = return_possible_positions(tic_tac_toe_board)
+    tell_player("-"*49)
+    player_move = ask_player_for_move(possible_positions)
+    tic_tac_toe_board[player_move] = "X"
+    player_winner = is_winner?("X", tic_tac_toe_board, WINNING_POSITIONS)
+    unless player_winner
+      sleep 0.5
+      possible_positions = return_possible_positions(tic_tac_toe_board)
+      computer_move = possible_positions.keys.sample
+      tic_tac_toe_board[computer_move] = "O"
+      computer_winner = is_winner?("O", tic_tac_toe_board, WINNING_POSITIONS)
+    end
+  end until return_possible_positions(tic_tac_toe_board).empty? || player_winner || computer_winner
+  
+  system "clear"
+  sleep 0.25
+  tell_player("#{"-"*20}GAME OVER#{"-"*20}")
+  if player_winner
+    tell_player("The computer doesn't know how to play. You won!")
+  elsif computer_winner
+    tell_player("The computer got lucky. You lost!!!")
+  else
+    tell_player("It's a tie.")
+  end
+  show_board(tic_tac_toe_board)
+  
+  tell_player("Do you want to play again? y/n")
+  play_again = gets.chomp.downcase == 'y'
+end while play_again
